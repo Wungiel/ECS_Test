207c5ae6eb11b22f88c26eb88d17eea5521c49db
Rdulemba <rdulemba@gmail.com>
2019-06-07 00:50:03 +0200

    Spawn system created, spawns unit in waves

---
+++ Assets/Scripts/Components/SpawnData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnData : MonoBehaviour
+{
+    public GameObject spawnedObject;
+    public float timeBetweenSpawns;
+    public float timeBetweenWaves;
+    public float timePassed;
+    public Direction initialDirection;
+    [Range(1,10)] public int waveLenght;
+    public int currentWaveCount;
+
+    public void OnValidate()
+    {
+        if (timeBetweenSpawns < 0)
+            timeBetweenSpawns = 1.0f;
+        if (timeBetweenWaves < 0)
+            timeBetweenWaves = 1.0f;
+        timePassed = 0;
+        currentWaveCount = 0;
+    }
+
+    public void spawn()
+    {
+        GameObject tmp = Instantiate(spawnedObject, this.transform.position, this.transform.rotation, this.transform);
+        tmp.GetComponent<MoveData>().direction = initialDirection;
+    }
+}
---
+++ Assets/Scripts/Systems/SpawnSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class SpawnSystem : ComponentSystem
+{
+    struct Components
+    {
+        public SpawnData spawnData;
+    }
+
+    protected override void OnUpdate()
+    {
+        foreach(var e in GetEntities<Components>())
+        {
+            if (e.spawnData.currentWaveCount == e.spawnData.waveLenght)
+            {
+                if (e.spawnData.timePassed > e.spawnData.timeBetweenWaves)
+                {
+                    e.spawnData.currentWaveCount = 0;
+                    e.spawnData.timePassed = 0;
+                }
+            }
+            else if (e.spawnData.timePassed > e.spawnData.timeBetweenSpawns)
+            {
+                e.spawnData.spawn();
+                e.spawnData.currentWaveCount++;
+                e.spawnData.timePassed = 0;
+            }
+            e.spawnData.timePassed += Time.deltaTime;
+        }
+    }
+}
