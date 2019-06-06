09334cce51685c7e3cd8726e624acac58011f948
Rdulemba <rdulemba@gmail.com>
2019-06-06 23:18:20 +0200

    Waypoints created

---
+++ Assets/WaypointData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointData : MonoBehaviour
+{
+    [Serializable]
+    public enum Direction
+    {
+        left, right, up, down
+    }
+
+    public Direction direction;
+}
