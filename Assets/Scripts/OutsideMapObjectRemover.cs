8f197fba536a684ee6301663eddd9c2b2b424806
Rdulemba <rdulemba@gmail.com>
2019-06-07 03:01:33 +0200

    Added an object destroying everything that leaves map in incorrent way (i.e. by missing a waypoint or endpoint)

---
+++ Assets/Scripts/OutsideMapObjectRemover.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutsideMapObjectRemover : MonoBehaviour
+{
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag(StringList.layerEnemy))
+            Debug.Log("Enemy outside road, check waypoints");
+        Destroy(collision.gameObject);
+    }
+}
